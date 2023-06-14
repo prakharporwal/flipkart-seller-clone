3d42283a8488bd7d021692d3822ca359fb138b60
prakharporwal <49529937+prakharporwal@users.noreply.github.com>
2023-06-14 14:16:33 +0530

    deploy

---
+++ script.js
@@ -0,0 +1,6 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./worker.js")
+    .then(() => console.log("registered"))
+    .catch((err) => console.log("error registering", err));
+}
---
+++ worker.js
@@ -0,0 +1,34 @@
+const cacheName = "cache-v1";
+const resourcesToPreCache = ["/", "index.html", "bundle.js", "manifest.json"];
+
+// todo : need to open this
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(resourcesToPreCache))
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
+
+// self.addEventListener("activate", (e) => {
+//   e.waitUntil(
+//     caches.open(cacheName).then((cache) => cache.addAll(resourcesToPreCache))
+//   );
+// });
+
+self.addEventListener("push", (e) => {
+  console.log("hello");
+
+  e.waitUntil(
+    self.registration.showNotification("title", {
+      body: "Hey I am body",
+      tag: "tagging",
+    })
+  );
+});
