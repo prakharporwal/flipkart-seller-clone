c362b7fe8da333e2baaac0c1383065b9c85e941d
prakharporwal <49529937+prakharporwal@users.noreply.github.com>
2023-06-14 13:03:26 +0530

    copy script in package.json added

--- public/worker.js
+++ public/worker.js
@@ -1,17 +1,5 @@
 const cacheName = "cache-v1";
-const resourcesToPreCache = [
-  "/",
-  "index.html",
-  "script.js",
-  "style.css",
-  "manifest.json",
-  "https://static-assets-web.flixcart.com/fk-sp-static/images/welcome_potal_banner_Desktop_V2_02.svg",
-  "src_Pages_ServicesPage_jsx.bundle.js",
-  "src_Components_NavBar_tsx.bundle.js",
-  "src_Pages_FeeStructurePage_tsx.bundle.js",
-  "src_Pages_ServicesPage_jsx.bundle.js",
-  "bundle.js",
-];
+const resourcesToPreCache = ["/", "index.html", "bundle.js", "manifest.json"];
 
 // todo : need to open this
 self.addEventListener("install", (e) => {
@@ -28,14 +16,13 @@ self.addEventListener("fetch", (e) => {
   );
 });
 
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.open(cacheName).then((cache) => cache.addAll(resourcesToPreCache))
-  );
-});
+// self.addEventListener("activate", (e) => {
+//   e.waitUntil(
+//     caches.open(cacheName).then((cache) => cache.addAll(resourcesToPreCache))
+//   );
+// });
 
 self.addEventListener("push", (e) => {
-  debugger;
   console.log("hello");
 
   e.waitUntil(
